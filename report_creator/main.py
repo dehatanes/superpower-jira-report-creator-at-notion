0f412162874c0d56c1079caa76f8946697eb5843
dehatanes <debs.outsmart@gmail.com>
2022-07-22 16:41:29 -0300

    Refac main

--- report_creator/main.py
+++ report_creator/main.py
@@ -16,8 +16,7 @@ def main():
 
     properties_schema = get_properties_schema()
     template_content = get_template_content()
-    new_page_properties, new_page_children = generate_page_properties(properties_schema, template_content)       
-    # print(f'{new_page_properties}')
+    new_page_properties, new_page_children = build_page_properties_and_content(properties_schema, template_content)
     new_page_id = create_page(new_page_properties, new_page_children)
 
     print()
@@ -55,21 +54,25 @@ def get_prop_value(prop_name: str, prop: dict):
         resp = input(f"Insira o valore de '{prop_name}'.\n[Valores aceitos:{options}]: ")
         return {"name": resp}
 
-def get_child_value(prop_child_type: str, child_prop: dict) -> dict:
-    block = { "object": "block"}
-    block['type'] = prop_child_type
-    block[prop_child_type] = child_prop[prop_child_type]
-    return block
 
-def generate_page_properties(properties_schema: dict, get_page_content: dict) -> (dict, list):
+def build_child_payload(prop_child_type: str, child_prop: dict) -> dict:
+    return {
+        "object": "block",
+        "type": prop_child_type,
+        prop_child_type: child_prop[prop_child_type],
+    }
+
+
+def build_page_properties_and_content(properties_schema: dict, page_content: list) -> (dict, list):
     properties = {
         prop['id']: {prop['type']: get_prop_value(name, prop)}
         for name, prop in properties_schema.items() if get_prop_value(name, prop)
     }
     properties_children = []
-    for child in get_page_content:
+    for child in page_content:
         if child['type'] != 'image':
-            properties_children.append(get_child_value(child['type'], child))
-    return properties,properties_children
+            properties_children.append(build_child_payload(child['type'], child))
+    return properties, properties_children
+
 
 main()
